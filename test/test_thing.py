45b98e1b5e338f86e54b6f2f034f5adfb3320e39
Johnny Richard <johnny.camargo@metasix.com.br>
2014-10-21 19:48:55 -0200

    Add test support

---
+++ test/test_thing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import unittest
+#from app.models import Thing
+
+class ThingTest(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def testAssertTrue(self):
+    self.assertTrue(True)
+
+if __name__ == '__main__':
+  unittest.main()
